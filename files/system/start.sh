394dc97714c15fcb70a681bd4b1786893d64a5ea
Thomas Steinbach <thomas.steinbach@aikq.de>
2016-07-21 03:29:37 +0200

    add /dev/dri in start script

--- files/system/start.sh
+++ files/system/start.sh
@@ -6,6 +6,7 @@ docker run -it --rm \
   -v "$HOME/dwarf-fortress/save:/df_linux/data/save" \
   -v "$HOME/dwarf-fortress/movies:/df_linux/data/movies" \
   -v "$HOME/dwarf-fortress/init:/df_linux/data/init" \
+  --device /dev/dri \
   -e DISPLAY=:0.0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   thomass/dwarf-fortress run-dwarf-fortress
